ccc8da5880849b0d518dd67d0babb4cc06920036
Kartik Kanungo <kartik.kanungo@paytmmoney.com>
2021-07-31 18:32:22 +0530

    Add a rough draft of the logic

--- GameOf1024/GameLogic/LogicManager.swift
+++ GameOf1024/GameLogic/LogicManager.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-
+/*
+ DRAFT(In Progress)
+ 
+ Init: Provide a 4 X 4 matrix at any state of the game
+ 
+ Input: Direction and the matrix with the current state of the game
+ OutPut: Matrix after executing the direction move
+ 
+ Responsibilites of logic manager
+ 1. On getting direction as an input
+    1. remove spaces (spaces means matrix value = 0)
+    2. Check the neighbours value
+        1. if same merge and add
+            1. while merging source to destination:
+                Destination = source + destination
+                source = 0
+        2. else do not do anything
+    3. Remove spaces and Continue till the time when merge is not needed
+ 
+ */
 
