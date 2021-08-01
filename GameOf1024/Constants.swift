b9d5ec238c0a646807061260ff9b57eadb426cf4
Kartik Kanungo <kartik.kanungo@paytmmoney.com>
2021-08-02 03:59:51 +0530

    Move constants to other file

---
+++ GameOf1024/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  GameOf1024
+//
+//  Created by Kartik Kanungo on 02/08/21.
+//
+
+import Foundation
+
+enum Constants: String {
+    case winningMessage = "Congrats!"
+    case winningTitle = "woooohoooo"
+    case winningCTA = "Play Again!"
+    
+    case lostMessage = "Sorry :( You lost"
+    case lostTitle = "oh No! "
+    case lostCTA = "Cool! Let me try again"
+}
--- GameOf1024/ViewModel/BoardViewModel.swift
+++ GameOf1024/ViewModel/BoardViewModel.swift
@@ -143,13 +143,3 @@ extension BoardViewModel {
     }
 }
 
-enum Constants: String {
-    case winningMessage = "Congrats!"
-    case winningTitle = "woooohoooo"
-    case winningCTA = "Play Again!"
-    
-    case lostMessage = "Sorry :( You lost"
-    case lostTitle = "oh No! "
-    case lostCTA = "Cool! Let me try again"
-    
-}
