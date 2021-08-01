e2201a3e9cc9eceb2a88eb3ad37e2e393224aea3
Kartik Kanungo <kartik.kanungo@paytmmoney.com>
2021-08-01 13:14:31 +0530

    Add scores variable

--- GameOf1024/ViewModel/BoardViewModel.swift
+++ GameOf1024/ViewModel/BoardViewModel.swift
@@ -10,10 +10,14 @@ import Foundation
 struct BoardViewModel {
     private var logicManager: LogicManager
     private var board: GameBoard
+    private var goal: Int
+    private var currentScore: Int
     
-    init(logicManager: LogicManager = LogicManager()) {
+    init(logicManager: LogicManager = LogicManager(), goal: Int = 1024, currentScore: Int = 0) {
         self.logicManager = logicManager
         self.board = self.logicManager.getAFreshBoard()
+        self.goal = goal
+        self.currentScore = currentScore
     }
     
     public func getTheCurrentBoardState() -> GameBoard {
